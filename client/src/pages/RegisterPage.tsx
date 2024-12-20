6768b0577a66db4459d659e4ef0eb6b2fbce3117
denys <543499@student.saxion.nl>
2024-12-20 10:27:42 +0100

    Create frontend register logic

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,27 +1,95 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { Link, useNavigate } from "react-router-dom";
+import { BiLoaderAlt } from "react-icons/bi";
+import useAuth from "../store/useAuth";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
-  const [data, setData] = useState({
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, setFields] = useState({
     name: "",
     email: "",
     password: "",
   });
+
+  const { user } = useAuth();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFields((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        body: JSON.stringify({
+          email: fields.email,
+          name: fields.name,
+          password: fields.password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        setError(errorData.message);
+        throw new Error("Something went wrong");
+      }
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [navigate, user]);
+
   return (
-    <div className="flex items-center justify-center h-screen mx-auto">
+    <div className="flex items-center justify-center h-screen mx-auto relative">
+      {isLoading && (
+        <div className="absolute inset-0 bg-black/20 flex items-center justify-center z-50">
+          <BiLoaderAlt className="animate-spin" size={36} />
+        </div>
+      )}
       <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-hray-900 md:text-2xl">
             Register a new account
           </h1>
           <form className="space-y-4 md:space-y-4">
-            <Input type="text" placeholder="Name" />
-            <Input type="email" placeholder="Email" />
-            <Input type="password" placeholder="Password" />
-            <Button className="w-full" type="submit">
+            <Input
+              name="name"
+              type="text"
+              placeholder="Name"
+              onChange={handleChange}
+            />
+            <Input
+              name="email"
+              type="email"
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <Button className="w-full" type="submit" onClick={handleSubmit}>
               Register
             </Button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
@@ -33,6 +101,7 @@ const RegisterPage = () => {
                 Log in
               </Link>
             </p>
+            {error && <p className="text-sm text-red-600">{error}</p>}
           </form>
         </div>
       </div>
